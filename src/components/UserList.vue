6a3413961902513041ebf7a39bc6048222a75787
Francisco García <frangaren@usal.es>
2018-04-21 20:46:36 +0200

    Add UserList components

---
+++ src/components/UserList.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+#user-list
+    span(v-for="user in users", :key="user.id") {{user}}
+</template>
+
+<script>
+import config from '@/config'
+import axios from 'axios'
+
+export default {
+    data: function () {
+        return {
+            users: []
+        }
+    },
+    created: function () {
+        axios.get(config.api + 'users/')
+            .then(res => this.users = res.data)
+            .catch(err => console.error(err))
+    }
+}
+</script>
+
+<style>
+</style>
