88e273ae80fef439aef5dd91ad630c6b428fbd4f
Francisco García <frangaren@usal.es>
2018-04-22 11:47:04 +0200

    Add UserDetails component

---
+++ src/components/UserDetails.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+ul(id="user-details")
+    li
+        b Nombre de usuario: 
+        span {{user.username}}
+    li
+        b Name:
+        span {{user.name}}
+    li
+        b Email:
+        span {{user.email}}
+</template>
+
+<script>
+import axios from 'axios'
+import config from '@/config'
+
+export default {
+  data: function () {
+      return {
+          user: {
+              username: ''
+          }
+      }
+  },
+  created: function () {
+    axios.get(`${config.api}users/${this.$route.params.id}`)
+        .then(res => this.user = res.data)
+        .catch(err => console.error(err))
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import UserList from '@/components/UserList'
+import UserDetails from '@/components/UserDetails'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/users',
       name: 'UserList',
       component: UserList
+    },
+    {
+      path: '/users/:id',
+      name: 'UserDetails',
+      component: UserDetails
     }
   ]
 })
